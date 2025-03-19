dd0b9d3cf2abffa4e0180e91783fb67e78d9d549
Jack May <92079641+jackmay942149@users.noreply.github.com>
2025-03-19 14:11:13 +1000

    Added enemy base class and variants

---
+++ Source/IGB300_Geme/BasicMeleeEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyBase.h"
+#include "BasicMeleeEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IGB300_GEME_API ABasicMeleeEnemy : public AEnemyBase
+{
+	GENERATED_BODY()
+	
+};
