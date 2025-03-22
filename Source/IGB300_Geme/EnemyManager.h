da2366e1837dcb891c4c4ff5ab25a8f278df5ea8
Jack May <92079641+jackmay942149@users.noreply.github.com>
2025-03-23 08:23:46 +1000

    feat(enemy-manager): added enemy tracker to enemy manager

--- Source/IGB300_Geme/EnemyManager.h
+++ Source/IGB300_Geme/EnemyManager.h
@@ -9,6 +9,8 @@
 #include "GameFramework/Actor.h"
 #include "EnemyManager.generated.h"
 
+class AEnemyBase;
+
 UCLASS()
 class IGB300_GEME_API AEnemyManager : public AActor
 {
@@ -18,7 +20,12 @@ public:
 	// Sets default values for this actor's properties
 	AEnemyManager();
 
-	TMap<int32, FVector> spawnLocations;
+	UPROPERTY(VisibleAnywhere, Category = "Members")
+		TMap<int32, FVector> spawnLocations;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Members")
+		TMap<int32, AEnemyBase*> enemies;
+
 	AActor* player;
 
 	UPROPERTY(VisibleAnywhere, Category = "Members")
