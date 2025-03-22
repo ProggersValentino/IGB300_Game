01263c2252ca4902b7880f244cb88bb8847bba25
HaileyG123 <n11263997@qut.edu.au>
2025-03-22 10:53:36 +1000

    Revert "Merge branch 'dev' into hailey"
    This reverts commit 62bd33290db6ebbb5d85e0e3562ed80f61319dd9, reversing
    changes made to 5a060454817b4246b63e19eabae9138c8a07a6be.

--- Source/IGB300_Geme/IGB300_Geme.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "IGB300_Geme.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IGB300_Geme, "IGB300_Geme" );
