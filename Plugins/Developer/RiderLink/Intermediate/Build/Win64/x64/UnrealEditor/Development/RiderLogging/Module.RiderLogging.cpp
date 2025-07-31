bde62f84d9cf7f3298d6214707e3a48ac7254864
LaziLombax <LaziLombax747@gmail.com>
2025-07-31 22:29:27 +1000

    fixed hud and connected the new level to the main menu

--- Plugins/Developer/RiderLink/Intermediate/Build/Win64/x64/UnrealEditor/Development/RiderLogging/Module.RiderLogging.cpp
+++ Plugins/Developer/RiderLink/Intermediate/Build/Win64/x64/UnrealEditor/Development/RiderLogging/Module.RiderLogging.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82b5a690008ac77edca01c0a4d1775097a63a01d132896d923b721ecf2ada1d4
-size 330
+oid sha256:e50645fbab384073fe436a6452e0a8db7a9a95c28a3ec4592f3506c0affc8c14
+size 342
