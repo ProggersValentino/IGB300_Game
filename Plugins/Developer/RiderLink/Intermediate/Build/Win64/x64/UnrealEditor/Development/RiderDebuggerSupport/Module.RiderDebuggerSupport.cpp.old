a65ff17e5dc6f546084d8d0a1274e7432f9a8a07
HaileyG123 <130008625+HaileyG123@users.noreply.github.com>
2025-08-02 17:42:21 +1000

    steam achievements set up
    plugin needed

--- Plugins/Developer/RiderLink/Intermediate/Build/Win64/x64/UnrealEditor/Development/RiderDebuggerSupport/Module.RiderDebuggerSupport.cpp.old
+++ Plugins/Developer/RiderLink/Intermediate/Build/Win64/x64/UnrealEditor/Development/RiderDebuggerSupport/Module.RiderDebuggerSupport.cpp.old
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a2635860c26ccb0c90f42d7755d1c8f83ac50f695f3239953f7a6159711886a
-size 709
+oid sha256:fe26fa4815395a4eaadeaa20f68b4476b9fb62e84b99c977c0e887316130ba99
+size 739
--- Plugins/Developer/RiderLink/Intermediate/Build/Win64/x64/UnrealEditor/Development/RiderLink/Module.RiderLink.cpp
+++ Plugins/Developer/RiderLink/Intermediate/Build/Win64/x64/UnrealEditor/Development/RiderLink/Module.RiderLink.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e33db447a5b08f983684d8e70950444a14c2fb8e9a678ff4858ec5b7668be1b
-size 4995
+oid sha256:174a930abd62bb435f8f25a89cadf249cd7e20a27627b02f1019ef1ae010fc8a
+size 5490
