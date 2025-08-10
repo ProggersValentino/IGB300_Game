06847378fe147ce71b58406b62f6937c22134fb0
LaziLombax <LaziLombax747@gmail.com>
2025-08-10 13:17:08 +1000

    Fixed some merge issues and updated the loading UI

---
+++ Plugins/Developer/RiderLink/Intermediate/Build/Win64/x64/UnrealEditor/Development/RiderDebuggerSupport/Default.rc2.res
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f9caf5583d11bb2d1eb7bad747761b43b73af9d58645a0f7b1295e03d8b04c36
+size 24960
---
+++ Plugins/Developer/RiderLink/Intermediate/Build/Win64/x64/UnrealEditor/Development/RiderLC/Definitions.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b1bc9fee4069ad3feca77eb2994815ea820238457fcf6f9328c8aacd9d5d81e0
+size 12381
