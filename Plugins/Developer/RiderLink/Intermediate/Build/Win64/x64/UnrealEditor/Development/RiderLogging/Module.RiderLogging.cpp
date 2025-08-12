29c515eec470b451144f4c91c7aabe6d38f515cc
LaziLombax <114451202+LaziLombax@users.noreply.github.com>
2025-08-12 19:21:26 +1000

    UI Niagara Coins and Glory VFX

--- Plugins/Developer/RiderLink/Intermediate/Build/Win64/x64/UnrealEditor/Development/RiderLogging/Module.RiderLogging.cpp
+++ Plugins/Developer/RiderLink/Intermediate/Build/Win64/x64/UnrealEditor/Development/RiderLogging/Module.RiderLogging.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e50645fbab384073fe436a6452e0a8db7a9a95c28a3ec4592f3506c0affc8c14
-size 342
+oid sha256:c143fece94f683dea90a7be9a7e187ca430654ae8bef918fc045ebed1a0e5978
+size 382
