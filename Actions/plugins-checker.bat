2657a7bd61bc20e5b8e3126ec90072427c78b1ef
Jack May <92079641+jackmay942149@users.noreply.github.com>
2025-08-25 18:39:30 +1000

    github actions fix

--- Actions/plugins-checker.bat
+++ Actions/plugins-checker.bat
@@ -1,6 +1,6 @@
 echo off
 cd Tools/plugins-checker/
-plugin-checker.exe ModelingToolsEditorMode GameplayAbilities
+plugin-checker.exe ModelingToolsEditorMode GameplayAbilities BlueprintFileUtils BlueprintStats AnimationWarping BlendStack OnlineSubsystemSteam OnlineFramework NiagaraUIRenderer
 if %ERRORLEVEL% NEQ 0 (
 	echo plugin-checker paniced
 	cd ../../
